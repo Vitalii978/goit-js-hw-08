7022e33f8210cbf289e9d1cf01e5b763dc2f3e37
Vitalii Klymenko <Vitalii.Klymenko78@gmail.com>
2025-05-24 21:40:58 +0200

    home work 8

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const categoriesEl = document.querySelectorAll('.item');
-console.log(`Number of categories: ${categoriesEl.length}`);
-
-categoriesEl.forEach((item) => {
-  console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elements: ${item.lastElementChild.children.length}`);
-});
